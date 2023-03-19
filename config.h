a44e4648241d6caad189a64d9b930879bc1ff34a
Przemek Grondek <przemek@grondek.pl>
2023-03-19 17:26:46 +0100

    Configure Three Column Layout

--- config.h
+++ config.h
@@ -75,10 +75,12 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-    { "|||",      col },
+    { "=|=",      tcl},      /* three columns layout */
+    { "|||",      col },     /* column layout */
     { "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
+static const Layout* monocleLayout = &layouts[4];
 
 /* key definitions */
 #define MODKEY Mod4Mask
--- dwm.c
+++ dwm.c
@@ -1829,7 +1829,7 @@ fullscreen(const Arg *arg)
 {
 	if (selmon->showbar) {
 		for(last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
-		setlayout(&((Arg) { .v = &layouts[3] }));
+		setlayout(&((Arg) { .v = &monocleLayout }));
 	} else {
 		setlayout(&((Arg) { .v = last_layout }));
 	}
