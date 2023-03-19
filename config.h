e46e3b50169dccf42940e55298d8aa256f92a57e
Przemek Grondek <przemek@grondek.pl>
2023-03-19 21:29:16 +0100

    Configure cyclelayout

--- config.h
+++ config.h
@@ -80,6 +80,7 @@ static const Layout layouts[] = {
     { "|M|",      centeredmaster },
     { "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+    { NULL,       NULL},
 };
 static const Layout* monocleLayout = &layouts[5];
 
@@ -119,6 +120,8 @@ static Key keys[] = {
 //	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 //	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 //	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+    { MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },
+    { MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
@@ -168,7 +171,7 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button1,        cyclelayout,    {.i = -1 } },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          SHCMD("gsimplecal") },
