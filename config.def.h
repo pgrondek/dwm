4a230d362ae7e6b017fb98c56a5994ed74f8c0f4
mihirlad55 <mihirlad55@gmail.com>
2023-08-04 21:23:37 +0200

    [PATCH] Add IPC support through a unix socket
    This patch currently supports the following requests:
    * Run custom commands with arguments (similar to key bind functions)
    * Get monitor properties
    * Get all available layouts
    * Get available tags
    * Get client properties
    * Subscribe to tag change, client focus change, and layout change,
      monitor focus change, focused title change, and client state change
      events
    
    This patch includes a dwm-msg cli program that supports all of the
    above requests for easy integration into shell scripts.
    
    The messages are sent in a JSON format to promote integration to
    increase scriptability in languages like Python/JavaScript.
    
    The patch requires YAJL for JSON parsing and a system with epoll
    support. Portability is planned to be increased in the future.
    
    This patch is best applied after all other patches to avoid merge
    conflicts.
    
    For more info on the IPC implementation and how to send/receive
    messages, documentation can be found at
    https://github.com/mihirlad55/dwm-ipc

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "monospace:sipcsockpathize=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -130,5 +130,23 @@ static const Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
+static const char *ipcsockpath = "/tmp/dwm.sock";
+static IPCCommand ipccommands[] = {
+  IPCCOMMAND(  view,                1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  toggleview,          1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  tag,                 1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  toggletag,           1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  tagmon,              1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  focusmon,            1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  focusstack,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  zoom,                1,      {ARG_TYPE_NONE}   ),
+  IPCCOMMAND(  incnmaster,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  killclient,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  togglefloating,      1,      {ARG_TYPE_NONE}   ),
+  IPCCOMMAND(  setmfact,            1,      {ARG_TYPE_FLOAT}  ),
+  IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
+  IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
 };
 
