1231c386fc009cdfefe25680769da56b6f22910c
Przemek Grondek <przemek@grondek.pl>
2024-10-09 02:22:12 +0200

    Set rofi as new application launcher

--- config.h
+++ config.h
@@ -121,7 +121,8 @@ static const  Layout* monocleLayout = &layouts[4];
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray5, "-sb", col_gray2, "-sf", col_gray4, NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray5, "-sb", col_gray2, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-combi-modi", "window,drun,ssh,combi", "-show", "combi", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
