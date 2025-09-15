5dfa881c9f862f1f407b0c434cb335ac28abec45
Przemek Grondek <przemek@grondek.pl>
2025-09-15 20:03:38 +0200

    Add shortcut for filebrowser

--- config.h
+++ config.h
@@ -124,6 +124,7 @@ static const  Layout* monocleLayout = &layouts[4];
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray5, "-sb", col_gray2, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "rofi", "-combi-modi", "window,drun,ssh,combi", "-show", "combi", NULL };
+static const char *filebrowsercmd[] = { "rofi", "-show", "filebrowser", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
@@ -131,6 +132,7 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_f,      spawn,          {.v = filebrowsercmd } },
 	{ MODKEY,                       XK_b,      spawn,          SHCMD("bt-menu") },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
     { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
